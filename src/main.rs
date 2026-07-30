38c4d5d8cca1c7a446840c8169dc989a5b736769
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2026-07-30 17:04:46 -0300

    add year 2024 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use std::{fs::read_to_string, time::Instant};
 
-use aoc_rs::year2025;
+use aoc_rs::year2024;
+use z3::DatatypeAccessor;
 
 fn main() {
-    let data = read_to_string("inputs/year2025/day11.txt").unwrap();
+    let data = read_to_string("inputs/year2024/day1.txt").unwrap();
 
-    let data = year2025::day11::parse(&data);
+    let data = year2024::day1::parse(&data);
 
     let start = Instant::now();
-    let solution_part_1 = year2025::day11::part_1(&data);
+    let solution_part_1 = year2024::day1::part_1(&data);
     let duration_part_1 = start.elapsed();
     println!(
         "Day 10 - Part 1 solution {} - elapsed {:#?}",
         solution_part_1, duration_part_1
     );
 
-    // let input = year2025::day06::parse_part2(&data);
     let start = Instant::now();
-    let solution_part_2 = year2025::day11::part_2(&data);
+    let solution_part_2 = year2024::day1::part_2(&data);
     let duration_part_2 = start.elapsed();
     println!(
         "Day 10 - Part 2 solution {} - elapsed {:#?}",
---
+++ src/year2024/day1.rs
@@ -0,0 +1,38 @@
+use rustc_hash::FxHashMap;
+
+pub fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let mut left = Vec::with_capacity(input.lines().count());
+    let mut right = Vec::with_capacity(left.capacity());
+    for l in input.lines() {
+        let mut it = l.split_whitespace();
+        let l_str = unsafe { it.next().unwrap_unchecked() };
+        let r_str = unsafe { it.next().unwrap_unchecked() };
+        left.push(unsafe { l_str.parse::<u32>().unwrap_unchecked() });
+        right.push(unsafe { r_str.parse::<u32>().unwrap_unchecked() });
+    }
+    (left, right)
+}
+
+pub fn part_1(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    let (mut left_list, mut right_list) = input.clone();
+    left_list.sort_unstable();
+    right_list.sort_unstable();
+
+    left_list
+        .iter()
+        .zip(right_list.iter())
+        .map(|(l, r)| u32::abs_diff(*l, *r))
+        .sum()
+}
+
+pub fn part_2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    let (left, right) = input;
+
+    let freq = {
+        let mut freq = FxHashMap::default();
+        right.iter().for_each(|r| *freq.entry(r).or_insert(0) += 1);
+        freq
+    };
+
+    left.iter().filter_map(|l| freq.get(l).map(|f| l * f)).sum()
+}
